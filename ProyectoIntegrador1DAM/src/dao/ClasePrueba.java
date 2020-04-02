a35c8067c56fa3dc69103c17c60e190df4e770e7
sagevit <jm.dios.martin@gmail.com>
2020-04-02 21:25:31 +0200

    Incluyo un campo en la clase

--- ProyectoIntegrador1DAM/src/dao/ClasePrueba.java
+++ ProyectoIntegrador1DAM/src/dao/ClasePrueba.java
@@ -2,4 +2,14 @@ package dao;
 
 public class ClasePrueba {
 
+	private String campo;
+
+	private String getCampo() {
+		return campo;
+	}
+
+	private void setCampo(String campo) {
+		this.campo = campo;
+	}
+	
 }
