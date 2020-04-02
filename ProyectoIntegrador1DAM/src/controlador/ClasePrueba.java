b03a623436eb0eadeeebf6021f12f8dbee459908
Esteban Martinez <esteban.mq09@gmail.com>
2020-04-02 21:50:53 +0200

    Incluyo metodo hola()

--- ProyectoIntegrador1DAM/src/controlador/ClasePrueba.java
+++ ProyectoIntegrador1DAM/src/controlador/ClasePrueba.java
@@ -3,6 +3,6 @@ package controlador;
 public class ClasePrueba {
 	//Prueba de guardado
 	public static void hola() {
-		System.out.format("%s\n", "Prueba github EMMQ");
+		System.out.format("%s\n", "Prueba github EMQ");
 	}
 }
