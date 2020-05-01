8e7a04ad67a13e87c80a192e114e0115040c20ea
Esteban Martinez <esteban.mq09@gmail.com>
2020-05-01 18:37:31 +0200

    Quitadas las clases de prueba

--- ProyectoIntegrador1DAM/src/controlador/ClasePrueba.java
+++
@@ -1,8 +0,0 @@
-package controlador;
-
-public class ClasePrueba {
-	//Prueba de guardado
-	public static void hola() {
-		System.out.format("%s\n", "Prueba github EMJQ");
-	}
-}
--- ProyectoIntegrador1DAM/src/modelo/ClasePrueba.java
+++
@@ -1,6 +0,0 @@
-package modelo;
-
-public class ClasePrueba {
-
-	//Esto es una prueba para ver si se guarda bien o no
-} 
