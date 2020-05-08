70b3a327b5d9b848c4c538548bb308473310bb6d
Jose Manuel de Dios <jm.dios.martin@gmail.com>
2020-05-08 12:28:56 +0200

    Quito el warning de CrearBD.java

--- ProyectoIntegrador1DAM/src/crearDB/CrearBD.java
+++ ProyectoIntegrador1DAM/src/crearDB/CrearBD.java
@@ -1,9 +1,8 @@
-package crearDB;
+	package crearDB;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class CrearBD {
 	
