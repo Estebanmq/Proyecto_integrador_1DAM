b7e13f9d22a0b730c0bd0893c4a9e357a1813f3f
sagevit <sagevit@sagevit-PC>
2020-05-16 01:53:09 +0200

    Elimino el script

--- ProyectoIntegrador1DAM/Scripts/creaVistaListaParticipante.sql
+++
@@ -1,2 +0,0 @@
-CREATE VIEW ListaParticipante (codigo, nombre, nacionalidad) AS
-	SELECT participante.codigo, participante.nombre, pais.descripcion FROM participante INNER JOIN pais on participante.nacionalidad = pais.codigo; 
