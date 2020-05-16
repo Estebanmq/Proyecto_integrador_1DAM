fafa24f329d9a19978fc87e9baad78b95c22baa1
Esteban Martinez <esteban.mq09@gmail.com>
2020-05-16 14:51:30 +0200

    Primera version de la ventana de DialogoAltaPelicula

--- ProyectoIntegrador1DAM/src/controlador/CtrlPeliculaAlta.java
+++ ProyectoIntegrador1DAM/src/controlador/CtrlPeliculaAlta.java
@@ -2,7 +2,12 @@ package controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
+
+import dao.DaoDirectorMantenimiento;
+import dao.DaoPaisMantenimiento;
 import modelo.Pelicula;
 import vista.DialogoPeliculaAlta;
 
@@ -12,22 +17,34 @@ public class CtrlPeliculaAlta implements ActionListener {
 	
 	private DialogoPeliculaAlta dialogoAltaPelicula;
 	
+	private DaoPaisMantenimiento daoPaisMantenimiento;
+	private DaoDirectorMantenimiento daoDirectorMantenimiento;
+	
 	public CtrlPeliculaAlta() {
+		daoPaisMantenimiento = new DaoPaisMantenimiento();
+		daoDirectorMantenimiento = new DaoDirectorMantenimiento();
 		
 		dialogoAltaPelicula = new DialogoPeliculaAlta();
 		dialogoAltaPelicula.getPanelBtnsAceptarCancelar().getBtnAceptar().addActionListener(this);
 		dialogoAltaPelicula.getPanelBtnsAceptarCancelar().getBtnCancelar().addActionListener(this);
+		try {
+			dialogoAltaPelicula.mostrarPaises(daoPaisMantenimiento.obtenerListaPaises());
+			dialogoAltaPelicula.mostrarDirectores(daoDirectorMantenimiento.obtenerNombreDirectores());
+		} catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error de conexión.", "Error", JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+        }
 		dialogoAltaPelicula.setVisible(true);
 		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
 		switch (e.getActionCommand()) {
 		case "btnAceptar" :
 			comprobarTitulo(); //Llamada a comprobarTitulo
 			System.out.format("Ha pulsado: %s\n", e.getActionCommand());
+			
 			//pelicula.setTitulo(dialogoAltaPelicula.getFieldTitulo().getText());
 			break;
 		case "btnCancelar" :
@@ -38,14 +55,18 @@ public class CtrlPeliculaAlta implements ActionListener {
 	}
 	
 	public void comprobarTitulo() {
+		String mensaje="";
 		System.out.format("%s\n", dialogoAltaPelicula.getFieldTitulo().getText());
 		if (dialogoAltaPelicula.getFieldTitulo().getText().equals("")) {
-			//dialogoAltaPelicula.mensajeError("El titulo no debe de estar vacio");
-			dialogoAltaPelicula.getPanelBtnsAceptarCancelar().getLabelTextoError().setText("Titulo no puede estar vacio");
-		} 
-//		if (dialogoAltaPelicula.get) {
-//			
-//		}
+			mensaje = "Titulo no puede estar vacio";
+		} else if (dialogoAltaPelicula.getComboBoxPais().getSelectedItem().equals("--Seleccionar País--")) {
+			mensaje = "Tiene que seleccionar un pais valido";
+		} else if (dialogoAltaPelicula.getComboBoxDirector().getSelectedItem().equals("--Seleccionar Director--")) {
+			mensaje = "Tiene que seleccionar un director valido";
+		} else if (dialogoAltaPelicula.getComboBoxGenero().getSelectedItem().equals("--Seleccionar Género--")) {
+			mensaje = "Tiene que seleccionar un genero valido";
+		}
+		dialogoAltaPelicula.getPanelBtnsAceptarCancelar().getLabelTextoError().setText(mensaje);
 	}
 
 	public DialogoPeliculaAlta getDialogoAltaPelicula() {
@@ -62,6 +83,14 @@ public class CtrlPeliculaAlta implements ActionListener {
 
 	public void setPelicula(Pelicula pelicula) {
 		this.pelicula = pelicula;
+	}
+	
+	public DaoPaisMantenimiento getDaoPaisMantenimiento() {
+		return daoPaisMantenimiento;
+	}
+
+	public void setDaoPaisMantenimiento(DaoPaisMantenimiento daoPaisMantenimiento) {
+		this.daoPaisMantenimiento = daoPaisMantenimiento;
 	}
 
 }
